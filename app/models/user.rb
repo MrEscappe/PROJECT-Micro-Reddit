9e6058cab29b049c626b9c8f67e738f4ac336f10
José Sérgio <sergiosilva@outlook.com>
2022-08-17 22:36:55 -0300

    Post and User finished, working on comments

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+class User < ApplicationRecord
+  has_many :posts
+  validates :username, length: { in: 3..15 }, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, length: { in: 4..12 }, presence: true
+end
