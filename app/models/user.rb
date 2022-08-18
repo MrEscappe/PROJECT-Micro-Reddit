167b4dd681b29390332c4190ff115c12c726bcbc
José Sérgio <sergiosilva@outlook.com>
2022-08-18 18:24:23 -0300

    finished project

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@
 
 class User < ApplicationRecord
   has_many :posts
+  has_many :comments
   validates :username, length: { in: 3..15 }, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
   validates :password, length: { in: 4..12 }, presence: true
